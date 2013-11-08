c4e42d734fdcdd5e1114b68075289f69394404b2
Ryan Cheung <ryanc@livefyre.com>
2013-11-07 20:08:33 -0800

    Add build profile

---
+++ build-prod.js
@@ -0,0 +1,29 @@
+({
+  mainConfigFile: './requirejs.conf.js',
+  paths: {
+    jquery: 'lib/jquery/jquery.min',
+    almond: 'lib/almond/almond'
+  },
+  baseUrl: '.',
+  name: "streamhub-map",
+  exclude: ['streamhub-sdk', 'almond'],
+  stubModules: ['text', 'hgn', 'json'],
+  out: "./dist/streamhub-map.min.js",
+  namespace: 'Livefyre',
+  pragmasOnSave: {
+    excludeHogan: true
+  },
+  optimize: "uglify2",
+  uglify2: {
+    compress: {
+      unsafe: true
+    },
+    mangle: true
+  },
+  onBuildRead: function(moduleName, path, contents) {
+    if (moduleName == "jquery") {
+      contents = "define([], function(require, exports, module) {" + contents + "});";
+    }
+    return contents;
+  }
+})
