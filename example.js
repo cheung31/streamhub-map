01785b92c5a952e8735012083041891b2603bd7e
Ryan Cheung <ryanc@livefyre.com>
2013-11-09 13:11:45 -0800

    Fix bug with map example. Now includes definition of lat/lon coords

--- example.js
+++ example.js
@@ -12,6 +12,13 @@ define(function(require) {
         // The Overlay API
         mapView.addOverlay(new SolarTerminatorOverlayView());
 
+        var sfLatLon = { lat: 37.7749295, lon: -122.4194155 };
+        var usaLatLon = { lat: 37.09024, lon: -95.712891 };
+        var europeLatLon = { lat: 54.5259614, lon: 15.2551187 };
+        var southAmericaLatLon = { lat: -8.783195, lon: -55.491477 };
+        var indiaLatLon = { lat: 20.593684, lon: 78.96288 };
+        var indonesiaLatLon = { lat: -0.789275, lon: 113.921327 };
+
         // Collection to Location Mapping
         //TODO(ryanc): collectionId -> lat/lon or articleId -> lat/lon
         var collectionToLocation = {
