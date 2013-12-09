786bb1220973d6343bbf5a1fd91fd4d16a1d67fd
Ryan Cheung <ryanc@livefyre.com>
2013-12-09 10:28:05 -0800

    Update app gallery example. Bump to v0.1.2

--- example.js
+++ example.js
@@ -1,39 +1,22 @@
 define(function(require) {
     var View = require('streamhub-map/collection/collection-map-view');
-    var SolarTerminatorOverlayView = require('streamhub-map/views/solar-terminator-view');
+    var Collection = require('streamhub-sdk/collection');
     var MockHotCollectionsStream = require('streamhub-hot-collections-tests/mocks/streams/mock-hot-collections');
 
     return function(el) {
-
-        var sfLatLon = { lat: 37.7749295, lon: -122.4194155 };
-        var usaLatLon = { lat: 37.09024, lon: -95.712891 };
-        var europeLatLon = { lat: 54.5259614, lon: 15.2551187 };
-        var southAmericaLatLon = { lat: -8.783195, lon: -55.491477 };
-        var indiaLatLon = { lat: 20.593684, lon: 78.96288 };
-        var indonesiaLatLon = { lat: -0.789275, lon: 113.921327 };
-
-        // Collection to Location Mapping
-        //TODO(ryanc): collectionId -> lat/lon or articleId -> lat/lon
-        var collectionToLocation = {
-            47993636: sfLatLon,
-            48084932: usaLatLon,
-            48078130: europeLatLon,
-            48081257: southAmericaLatLon,
-            48089894: indiaLatLon,
-            48103282: indonesiaLatLon
+        var opts = {
+            "network": "livefyre.com",
+            "siteId": "290596",
+            "articleId": "172",
+            "environment": "qa-ext.livefyre.com"
         };
+        var collection = new Collection(opts);
+
         var mapView = new View({
-            el: el,
-            collectionToLocation: collectionToLocation
+            el: el
         });
 
-        // The Overlay API
-        mapView.addOverlay(new SolarTerminatorOverlayView());
-
-        var hotCollectionsStream = new MockHotCollectionsStream({
-            network: 'livefyre.com'
-        });
-        hotCollectionsStream.pipe(mapView);
+        collection.pipe(mapView);
   
         return view;
     };
