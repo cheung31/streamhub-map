773d2ea33d9889c9c5722aa527ea7031be1515b5
Ryan Cheung <ryanc@livefyre.com>
2014-01-16 13:16:50 -0800

    Update app gallery example

--- example.js
+++ example.js
@@ -4,15 +4,18 @@ define(function(require) {
 
     return function(el) {
         var opts = {
-            "network": "livefyre.com",
-            "siteId": "290596",
-            "articleId": "172",
-            "environment": "qa-ext.livefyre.com"
+            "network": "strategy-prod.fyre.co",
+            "siteId": "340628",
+            "articleId": "custom-1389845009515"
         };
         var collection = new Collection(opts);
 
         var view = new View({
             el: el
+            ,center: [37.774929499038386, -122.41941549873445]
+            ,zoom: 13
+            ,maxZoom: 16
+            ,cloudmadeStyleId: 77922
         });
 
         collection.pipe(view);
